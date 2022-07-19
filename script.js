cd08a7171121c7cd75042643104f339ed9d7a7f1
EvanKluger <jakeykluger@gmail.com>
2022-07-19 10:19:59 -0400

    added second level

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let banana = document.querySelector('.Banana');
 let trash = document.querySelector('.Trash');
 let monkey = document.querySelector('.Monkey');
 let jungle = document.querySelector('.Jungle');
+let banana2 = document.querySelector('.Banana2');
+let trash2 = document.querySelector('.Trash2');
 let score = 0;
 let moveBy = 30;
 let step = 1;
@@ -21,7 +23,14 @@ trash.style.left = '800px';
 trash.style.top = '200px';
 let trash_int_top = 150;
 let trash_int_left = 800;
-let count_speed = 0
+let count_speed = 0;
+let count_items = 0;
+banana2.style.left = '400px';
+banana2.style.top = '200px';
+let banana2_int_top = 150;
+trash2.style.left = '800px';
+trash2.style.top = '200px';
+let trash2_int_top = 150;
 
 
 
@@ -39,9 +48,66 @@ function update(){
 function faster(){
     if(count_speed == 5){
         step = step + 1;
-        moveBy = moveBy + 3;
+        moveBy = moveBy + 5;
         count_speed = 0;
     }
+}
+
+function add_items(){
+    if(count_items > 10){
+        banana2.style.height = '70px';
+        banana2.style.width = '70px';
+    
+        trash2.style.height = '70px';
+        trash2.style.width = '70px';
+
+        if(banana2_int_top < 850){
+            banana2.style.top = parseInt(banana2.style.top) + step + 'px';
+            banana2_int_top = banana2_int_top + step;
+        }
+        if(banana2_int_top >= 850){
+        banana2.style.left = random_x() + 'px'
+        banana2_int_top = 200
+        banana2.style.top = '200px';
+        }
+    
+        if(trash2_int_top < 850){
+            trash2.style.top = parseInt(trash2.style.top) + step + 'px';
+            trash2_int_top = trash2_int_top + step;
+        }
+        if(trash2_int_top >= 850){
+            trash2.style.left = random_x() + 'px';
+            trash2_int_top = 200;
+            trash2.style.top = '200px';
+        }
+    
+        if((parseInt(banana2.style.left) - 70 < parseInt(monkey.style.left)) && (parseInt(monkey.style.left) < parseInt(banana2.style.left) + 70) && (parseInt(banana2.style.top) - 70 < parseInt(monkey.style.top)) && (parseInt(monkey.style.top) < parseInt(banana2.style.top) + 70) ){
+            score = score + 1;
+            count_speed = count_speed + 1;
+            banana2.style.left = random_x() + 'px'
+            banana2_int_top = 200
+            banana2.style.top = '200px';
+        }
+        if((parseInt(trash2.style.left) - 70 < parseInt(monkey.style.left)) && (parseInt(monkey.style.left) < parseInt(trash2.style.left) + 70) && (parseInt(trash2.style.top) - 70 < parseInt(monkey.style.top)) && (parseInt(monkey.style.top) < parseInt(trash2.style.top) + 70) ){
+            alert('GAME OVER')
+            score = 0;
+            banana2.style.left = random_x() + 'px'
+            banana2_int_top = 200
+            banana2.style.top = '200px';
+            trash2.style.left = random_x() + 'px';
+            trash2_int_top = 200;
+            trash2.style.top = '200px';
+
+            banana.style.left = random_x() + 'px'
+            banana_int_top = 200
+            banana.style.top = '200px';
+            trash.style.left = random_x() + 'px';
+            trash_int_top = 200;
+            trash.style.top = '200px';
+            clearInterval(interval)
+        }
+        }
+
 }
 
 //FUNCTION TO CONTROL THE MONEKY'S MOVEMENT
@@ -127,6 +193,7 @@ function start_game(){
         setTimeout(game(), 1);
         update();
         faster();
+        add_items();
         }, 25);
     
     
